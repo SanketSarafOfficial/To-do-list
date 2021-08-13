53a51ce15827d39f37b61353277796b5752ed5b9
sanket <sanket.saraf.official@gmail.com>
2021-08-13 20:36:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ()=>
+{
 
+
+      const [inputList , setInputList] = useState("");
+      
+      const [items  ,setItems] = useState([]); // made an empty array and called items in the ol
+
+
+      const inputEvent =(event)=>
+      {
+       setInputList(event.target.value);
+      };
+
+
+      const listOfItems =()=>
+      {
+            setItems((oldItems)=>
+            {
+                  return [...oldItems, inputList] //store the elements inthe usestate and save it to old items and display it and the last element inputList
+            }
+            );
+            setInputList(" ");
+      }
+
+      const deleteItems = ()=>
+      {
+            console.log("deleted");
+            setItems((oldItems)=>
+            {
+                  return oldItems.filter((array , index)=>{
+                              return index;
+
+                  })
+            })
+      }
+
+
+      const deleteAllItems = (i)=>
+      {
+            console.log("deleted All");
+            setItems((oldItems)=>
+            {
+                  return oldItems.filter((array , index)=>{
+                              return index[i];
+
+                  })
+            })
+      }
+
+
+
+      return (<>  
+      
+      <div className="main_div">
+
+            <div className="center_div">
+
+                 <br/>
+
+                 <h1 class="todo"> To Do List </h1>
+      
+                 <input type="text" className="inputbox" name="" value={inputList} placeholder="Add your items" onChange={inputEvent} />
+                 <button className="button btn btn-success btn-lg" onClick={listOfItems}> Add </button>
+                 <button className="button2 btn btn-danger btn-lg"  onClick={deleteItems}> Delete </button>
+                 <button className="button3 btn btn-warning btn-lg"  onClick={deleteAllItems}> Delete All</button>
+
+
+                   <div className="list">
+
+                               <ul >
+
+                               {/*  */}
+
+                               {items.map( (itemval , index)=>{
+                                     
+                                     return <li>{itemval}</li>
+                                     
+                                     
+                                     })}
+
+                               </ul>
+
+                   </div>
+
+            </div>
+
+            </div>
+
+
+
+
+
+      
+       </>
+
+      );
+}
 export default App;
+
+
+// <>  </> -> react fragment 
